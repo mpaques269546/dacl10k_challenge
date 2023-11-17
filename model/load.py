dc0f66644e802861010fc43b7214f05ba04b40e5
Matthieu Paques <98837383+mpaques269546@users.noreply.github.com>
2023-11-17 15:20:50 +0100

    Update load.py

--- model/load.py
+++ model/load.py
@@ -16,9 +16,9 @@ def load_segmenter(pretrained_weights):
 	img_size = 512
 	activation = torch.nn.Identity()
 	encoder = build_model(pretrained_weights,  key='encoder', trainable=False, embed_dim=1024, 
-	                                arch='beit_large_adapter', patch_size=16,  image_size=img_size, num_queries=10)
+	                                arch='beit_large_adapter', patch_size=16,  image_size=img_size)
 	decoder = build_model(pretrained_weights, key='decoder', arch='mask2former', trainable=False, 
-	 num_cls=num_cls, embed_dim=encoder.feat_dim, image_size=img_size, activation=activation)
+	 num_cls=num_cls, embed_dim=encoder.feat_dim, image_size=img_size, activation=activation, num_queries=10)
 	segmenter = Segmenter( encoder, decoder, pred_size = img_size )
 	return segmenter.eval()
 
